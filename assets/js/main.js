d872bf4ee4638771780e1f95ec581281a92c5acb
AdnanPhy <85302584+AdnanPhy@users.noreply.github.com>
2023-01-08 15:27:34 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,29 +15,18 @@ $(window).resize(sectionHeight);
 $(function() {
   $("section h1, section h2, section h3").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $(this).attr('target','_self',$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
-    e.preventDefault();
-    var position = $(this).attr("href"));
+    var position = $($(this).attr('target','_self')).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
-    window.open(url, '_blank');
+    event.preventDefault();
   });
 
-  $(document).ready(function(){
-        $('a[href^="http://"]').each(function(){ 
-            var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("http://", "https://"); // Create new url
-            $(this).attr("href", newUrl); // Set herf value
-        });
-    });
-  
-  
-  
   sectionHeight();
 
   $('img').on('load', sectionHeight);
