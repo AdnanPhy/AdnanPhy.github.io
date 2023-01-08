f0e0aa5f6148b7b72857ff213ff13b7d0e2d1cba
AdnanPhy <85302584+AdnanPhy@users.noreply.github.com>
2023-01-08 13:25:14 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,16 @@ $(function() {
     event.preventDefault();
   });
 
+  $(document).ready(function(){
+        $('a[href^="http://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("http://", "https://"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+  
+  
+  
   sectionHeight();
 
   $('img').on('load', sectionHeight);
