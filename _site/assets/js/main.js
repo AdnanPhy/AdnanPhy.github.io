8486533ff22bef1f1aebde6ccbc4cc877f578b49
AdnanPhy <85302584+AdnanPhy@users.noreply.github.com>
2023-01-08 15:38:09 -0500

    Update main.js

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -15,12 +15,12 @@ $(window).resize(sectionHeight);
 $(function() {
   $("section h1, section h2, section h3").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $(this).attr('target','_self',$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
+    var position = $($(this).attr('target','_self')).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
