71750ef6a5f5f1074963a6d694b832ecf6e57a66
AdnanPhy <85302584+AdnanPhy@users.noreply.github.com>
2023-01-08 12:49:25 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(function() {
-  $("section h1, section h2, section h3").each(function(){
+  $("section h1, section h2, section h3").on("click", "a", function(event){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("href",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
